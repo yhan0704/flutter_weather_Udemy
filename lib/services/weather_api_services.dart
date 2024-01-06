f88a5ac10ca0500e0f0c4e70d8d622a01f0c0091
yhan0704 <hanyc1205@gmail.com>
2024-01-06 10:20:03 -0500

    API-2

--- lib/services/weather_api_services.dart
+++ lib/services/weather_api_services.dart
@@ -5,6 +5,7 @@ import 'package:http/http.dart' as http;
 import 'package:open_weather_cubit/constants/constants.dart';
 import 'package:open_weather_cubit/exceptions/weather_exception.dart';
 import 'package:open_weather_cubit/models/direct_geocoding.dart';
+import 'package:open_weather_cubit/models/weather.dart';
 import 'package:open_weather_cubit/services/http_error_handler.dart';
 
 class WeatherApiServices {
@@ -41,6 +42,36 @@ class WeatherApiServices {
       final directGeocoding = DirectGeocoding.fromJson(responseBody);
 
       return directGeocoding;
+    } catch (e) {
+      rethrow;
+    }
+  }
+
+  Future<Weather> getWeather(DirectGeocoding directGeocoding) async {
+    final Uri uri = Uri(
+      scheme: 'https',
+      host: kApiHost,
+      path: '/data/2.5/weather',
+      queryParameters: {
+        'lat': '${directGeocoding.lat}',
+        'lon': '${directGeocoding.lon}',
+        'units': kUnit,
+        'appid': dotenv.env['APPID'],
+      },
+    );
+
+    try {
+      final http.Response response = await httpClient.get(uri);
+
+      if (response.statusCode != 200) {
+        throw Exception(httpErrorHandler(response));
+      }
+
+      final weatherJson = json.decode(response.body);
+
+      final Weather weather = Weather.fromJson(weatherJson);
+
+      return weather;
     } catch (e) {
       rethrow;
     }
